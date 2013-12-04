b0a70ac59134aed11e4b7f9d85ae9e3b76f22b08
Florian Metzger <florian.metzger@univie.ac.at>
2013-12-04 16:48:51 +0100

    serious fixes to the measurement series csv generator, hopefully should produce accurate results now

--- scripts/py-playbackemulation/results_csv_writer.py
+++ scripts/py-playbackemulation/results_csv_writer.py
@@ -4,22 +4,11 @@ Created on Thu Oct 13 09:18:44 2011
 
 @author: fm
 """
-
 import sys
 import os
 import os.path
-
 import csv
-
-import subprocess
 import string
-
-from numpy import *
-
-import matplotlib.pyplot as plt
-
-import pylab
-
 import ytresults_commandline as yt
 
 def calcstalls(stalls):
@@ -39,65 +28,42 @@ def calcstalls(stalls):
 			num = plcnt
 			pllen = pllen + (stalls[i].end-stalls[i].start)
 
-			print(str(i) + ": " + stalls[i].type + "=" + str(i) + "; start=" + str(stalls[i].start) + "; end:" + str(stalls[i].end) + "; duration="+str(stalls[i].end-stalls[i].start))
+		print(str(i) + ": " + stalls[i].type + "=" + str(i) + "; start=" + str(stalls[i].start) + "; end:" + str(stalls[i].end) + "; duration="+str(stalls[i].end-stalls[i].start))
 
-			print("")
-			print("num_stalls="+str(stcnt))
-			print("time_stall_total="+str(stlen)) 
-			print("num_playruns="+str(plcnt))
-			print("time_play_total="+str(pllen)) 
+	print("")
+	print("num_stalls="+str(stcnt))
+	print("time_stall_total="+str(stlen)) 
+	print("num_playruns="+str(plcnt))
+	print("time_play_total="+str(pllen)) 
 
-			return(stcnt,stlen,plcnt,pllen)
-
+	return(stcnt,stlen,plcnt,pllen)
 
 if __name__ == "__main__":
 
 	algs = ["ytfa","ffh5","nnbs","stbs"]
-
 	rootdir = sys.argv[1]
-	#subvideoid = sys.argv[2]
 
-		
-	# dirs = os.listdir(os.path.abspath(rootfolder))
-	vals = []
-
-	with open('playbackemulation-1.csv', 'wb') as f:
+	with open('playbackemulation.csv', 'wb') as f:
 		writer = csv.writer(f)
 		writer.writerow(["video_id", "video_duration", "strat", "video_width", "video_height", "video_bitrate", "qostype", "qosvalue", "duration", "stall_duration", "stall_count"])
 
 		for current_dir, subfolders, files in os.walk(rootdir):
 			if not subfolders: # only leaf dirs are interesting
-
 				for alg in algs:
 					for subvideoid in range(10):
 						print "Calculating values for " + current_dir
-
 						try:
 							dirname = string.split(current_dir, "/")[-1]
 							dirsplit = string.split(dirname,"_")
 							videoid = dirsplit[0]
 							qostype = dirsplit[1]
 							qosparam = dirsplit[2]
-
-						
 							myG = yt.Graphs(current_dir, subvideoid)
 							(bufferlevels, frame_timestamps, total_offset, stalls) = myG.stallingalgorithm(alg)
-							video_width = myG.video_width
-							video_height = myG.video_height
-							video_bitrate = myG.overall_bitrate
-							video_duration = myG.video_duration
-
 							(num_stalls, time_stalls, num_playruns, time_play) = calcstalls(stalls)
-							vals.append((videoid, video_duration, alg, video_width, video_height, video_bitrate, qostype, float(qosparam), time_play, time_stalls, num_stalls))
-							writer.writerows(vals)
-
+							writer.writerow([videoid, myG.video_duration, alg, myG.video_width, myG.video_height, myG.overall_bitrate, qostype, float(qosparam), time_play, time_stalls, num_stalls])
 						except Exception, e:
 							print e
-
-
-
-	
-		#vals = sorted(vals, key=lambda x: x[1]) # sort by first item in tuple: qosparam
 
 	   
 
