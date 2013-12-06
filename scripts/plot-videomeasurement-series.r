41e449fdb940eb6aac4762e056fb19e76f6587d9
Florian Metzger <fm@tru42.org>
2013-12-06 10:35:25 +0100

    adapt measurement section to new R eval and figures

--- scripts/plot-videomeasurement-series.r
+++ scripts/plot-videomeasurement-series.r
@@ -10,27 +10,42 @@ setwd("F:/uni/git/thesis/scripts/py-playbackemulation/")
 d <- read.csv("playbackemulation.csv", header=T)
 
 d$relativestall <- d$stall_duration/d$duration
-df <- subset(d, d$duration == d$video_duration)
+#df <- subset(d, d$duration == d$video_duration) # this fails due to float rounding errors, avoid!
+df <- subset(d, abs(d$duration - 92.5) < 1)
 
-dloss <- subset(df, df$qostype == "loss")
-dlatency <- subset(df, df$qostype == "delay")
+dmax <- subset(df, df$stall_duration < 1000)
+
+dloss <- subset(dmax, dmax$qostype == "loss")
+dlatency <- subset(dmax, dmax$qostype == "delay")
 
 pl <- ggplot(dloss, aes(x=qosvalue, y=relativestall, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("packet loss (%)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Null Strategy", "Predictive", "YouTube Flash")) + theme(text = element_text(size=20))
+pl + xlab("packet loss (%)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
 ggsave("R-playbackemulation-stallduration-loss.pdf")
 
 pl <- ggplot(dloss, aes(x=qosvalue, y=stall_count, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("packet loss (%)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Null Strategy", "Predictive", "YouTube Flash")) + theme(text = element_text(size=20))
+pl + xlab("packet loss (%)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
 ggsave("R-playbackemulation-stallnumber-loss.pdf")
 
 
 pl <- ggplot(dlatency, aes(x=qosvalue, y=relativestall, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("latency (ms)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Null Strategy", "Predictive", "YouTube Flash")) + theme(text = element_text(size=20))
+pl + xlab("latency (ms)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
 ggsave("R-playbackemulation-stallduration-latency.pdf")
 
 pl <- ggplot(dlatency, aes(x=qosvalue, y=stall_count, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("latency (ms)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Null Strategy", "Predictive", "YouTube Flash")) + theme(text = element_text(size=20))
+pl + xlab("latency (ms)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
 ggsave("R-playbackemulation-stallnumber-latency.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## old single strategy libreoffice import
