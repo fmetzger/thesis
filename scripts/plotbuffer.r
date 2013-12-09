7c7631466a4e4eff93f55c921cd2cd964103caac
Florian Metzger <florian.metzger@univie.ac.at>
2013-12-09 12:20:50 +0100

    corrected figure labels

--- scripts/plotbuffer.r
+++ scripts/plotbuffer.r
@@ -6,7 +6,7 @@ setwd("/home/fm/git/thesis/scripts/")
 d <- read.csv("buffer.csv", header=T)
 
 p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line(size = 1)
-p <- p + xlab("time (s)") + ylab("data (KiB)") + scale_color_discrete(name="",breaks=c("frame", "segment"), labels=c("received", "played")) + theme(text = element_text(size=20))
+p <- p + xlab("time (s)") + ylab("data (KiB)") + scale_color_discrete(name="",breaks=c("segment", "frame"), labels=c("received", "played")) + theme(text = element_text(size=20))
 p + annotate("text", x = 5, y = 5800, label = "detail plot") + geom_segment(aes(x = 5, y = 5600, xend = 5, yend = 4600), arrow = arrow(length = unit(0.5, "cm")), colour="black")
 #p + annotation_custom(grob=circleGrob(r=unit(1,"npc")), xmin=2, xmax=4, ymin=4000, ymax=4500) #  add circle on top for more highlighting
 ggsave("R-blocktransfer.pdf")
