21cb7db1cffb4669708a078677eb0bf453701146
Florian Metzger <florian.metzger@univie.ac.at>
2014-06-11 09:26:27 +0200

    chapter 3 figure font replace-al and color palette adjustments

--- scripts/plot-videomeasurement-series.r
+++ scripts/plot-videomeasurement-series.r
@@ -3,8 +3,10 @@
 
 library(Hmisc) #implicitly loaded by ggplot2 when required
 library(ggplot2)
+library(extrafont)
 
-d <- read.csv("playbackemulation.csv", header=T)
+d <- read.csv("/home/fm/git/thesis/scripts/py-playbackemulation/playbackemulation.csv", header=T,
+              colClasses=c("factor", "numeric", "factor", "factor", "factor", "factor", "factor", "numeric", "numeric", "numeric", "numeric", "factor"))
 
 d$relativestall <- d$stall_duration/d$duration
 #df <- subset(d, d$duration == d$video_duration) # this fails due to float rounding errors, avoid!
@@ -15,19 +17,38 @@ dmax <- subset(df, df$stall_duration < 1000)
 dloss <- subset(dmax, dmax$qostype == "loss")
 dlatency <- subset(dmax, dmax$qostype == "delay")
 
-pl <- ggplot(dloss, aes(x=qosvalue, y=relativestall, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("packet loss (%)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
-ggsave("R-playbackemulation-stallduration-loss.pdf")
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-pl <- ggplot(dloss, aes(x=qosvalue, y=stall_count, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("packet loss (%)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
-ggsave("R-playbackemulation-stallnumber-loss.pdf")
+p <- ggplot(dloss, aes(x=qosvalue, y=relativestall, color=as.factor(strat)))
+p <- p + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar")
+p <- p + xlab("packet loss (%)") + ylab("stall duration / video duration")
+p <- p + scale_color_manual(values=cbPalette, name="playback strategies", breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "predictive", "null strategy", "YouTube Flash"))
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p
+ggsave("R-playbackemulation-stallduration-loss.pdf", width=12, height=10, useDingbat=F)
 
 
-pl <- ggplot(dlatency, aes(x=qosvalue, y=relativestall, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("latency (ms)") + ylab("stall duration / video duration") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
-ggsave("R-playbackemulation-stallduration-latency.pdf")
+p <- ggplot(dloss, aes(x=qosvalue, y=stall_count, color=as.factor(strat)))
+p <- p  + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar")
+p <- p + xlab("packet loss (%)") + ylab("number of playback stalls")
+p <- p + scale_color_manual(values=cbPalette, name="playback strategies", breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "predictive", "null strategy", "YouTube Flash"))
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p
+ggsave("R-playbackemulation-stallnumber-loss.pdf", width=12, height=10, useDingbat=F)
 
-pl <- ggplot(dlatency, aes(x=qosvalue, y=stall_count, color=as.factor(strat))) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar") + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2)
-pl + xlab("latency (ms)") + ylab("number of playback stalls") + scale_color_discrete(name="Playback Strategies",breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "Predictive", "Null Strategy", "YouTube Flash")) + theme(text = element_text(size=20))
-ggsave("R-playbackemulation-stallnumber-latency.pdf")
+
+p <- ggplot(dlatency, aes(x=qosvalue, y=relativestall, color=as.factor(strat)))
+p <- p + stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar")
+p <- p + xlab("latency (ms)") + ylab("stall duration / video duration")
+p <- p + scale_color_manual(values=cbPalette, name="playback strategies", breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "predictive", "null strategy", "YouTube Flash"))
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p 
+ggsave("R-playbackemulation-stallduration-latency.pdf", width=12, height=10, useDingbat=F)
+
+p <- ggplot(dlatency, aes(x=qosvalue, y=stall_count, color=as.factor(strat)))
+p <- p+ stat_smooth(method="loess",  fullrange=T, size=1, se=T, lty=2) + stat_summary(fun.y = mean, geom="point", size=4) + stat_summary(fun.data="mean_cl_boot", geom="errorbar")
+p <- p + xlab("latency (ms)") + ylab("number of playback stalls")
+p <- p + scale_color_manual(values=cbPalette, name="playback strategies", breaks=c("ffh5", "nnbs", "stbs", "ytfa"), labels=c("Firefox 4", "predictive", "null strategy", "YouTube Flash"))
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p 
+ggsave("R-playbackemulation-stallnumber-latency.pdf", width=12, height=10, useDingbat=F)
--- scripts/plotbuffer.r
+++ scripts/plotbuffer.r
@@ -10,15 +10,21 @@ d$timestamp <- as.numeric(d$timestamp)
 d$size <- as.numeric(d$size)
 d$type <- as.factor(d$type)
 
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
 p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line(size = 1)
-p <- p + xlab("time (s)") + ylab("data (KiB)") + scale_color_discrete(name="",breaks=c("segment", "frame"), labels=c("received", "played"))
+p <- p + xlab("time (s)") + ylab("data (KiB)") 
 p <- p + annotate("text", x = 5, y = 5800, label = "detail plot", family="Liberation Sans Narrow") + geom_segment(aes(x = 5, y = 5600, xend = 5, yend = 4600), arrow = arrow(length = unit(0.5, "cm")), colour="black")
 p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p <- p + scale_color_manual(values=cbPalette, name="",breaks=c("segment", "frame"), labels=c("received", "played"))
 p
 #p + annotation_custom(grob=circleGrob(r=unit(1,"npc")), xmin=2, xmax=4, ymin=4000, ymax=4500) #  add circle on top for more highlighting
 ggsave("R-blocktransfer.pdf", width=12, height=10, useDingbat=F)
 
-p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line() +geom_point(size=2) + xlim(c(4.4,5.5)) + ylim(c(4350,4550))
-p + xlab("time (s)") + ylab("data (KiB)") +scale_color_discrete(guide="none")
+p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line(size = 1) +geom_point(size=3) + xlim(c(4.4,5.5)) + ylim(c(4350,4550))
+p <- p + xlab("time (s)") + ylab("data (KiB)")
 p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p <- p + scale_color_manual(values=cbPalette, guide="none")
+p                              
 ggsave("R-blocktransferdetail.pdf", width=12, height=10, useDingbat=F)
