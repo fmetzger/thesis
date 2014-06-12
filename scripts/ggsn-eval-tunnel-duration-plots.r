79362196d9d5308630d4d0c7c3f07338e4feeeda
Florian Metzger <florian.metzger@univie.ac.at>
2014-06-12 15:20:45 +0200

    layer timescales qualitative figure remade in R all R plots converted to new palette and (embedded) liberation font only 4*2 python plots remaining, with dejavu and bitstream vera embeds

--- scripts/ggsn-eval-tunnel-duration-plots.r
+++ scripts/ggsn-eval-tunnel-duration-plots.r
@@ -9,6 +9,7 @@ st_symbian <- scan("/home/fm/svn/ursa/out/duration_symbian", dec=".")
 st_ios <- scan("/home/fm/svn/ursa/out/duration_ios", dec=".")
 st_android <- scan("/home/fm/svn/ursa/out/duration_android", dec=".")
 
+
 # take 1% samples for easier handling
 servingtimes_sampled <- sample(servingtimes, length(servingtimes)*0.005) # take a sample to not need to work with the whole dataset
 st_dongle_sampled <- sample(st_dongle, length(st_dongle)*0.005)
@@ -41,6 +42,8 @@ ggsave("R-tunnel-duration-device-type.pdf", width=12, height=10, useDingbats=F)
 embed_fonts("R-tunnel-duration-device-type.pdf")
 
 
+
+
 df <- data.frame()
 x <- data.frame(st=servingtimes_sampled, type="total")
 df <- rbind(df,x)
@@ -61,4 +64,53 @@ p
 
 ggsave("R-tunnel-duration-operating-system.pdf", width=12, height=10, useDingbats=F)
 embed_fonts("R-tunnel-duration-operating-system.pdf")
+
+
+
+
+
+servingtimes_sampled <- sample(servingtimes, length(servingtimes)*0.1) # take a sample to not need to work with the whole dataset
+st_dongle_sampled <- sample(st_dongle, length(st_dongle)*0.1)
+st_regularphone_sampled <- sample(st_regularphone, length(st_regularphone)*0.1)
+st_smartphone_sampled <- sample(st_smartphone, length(st_smartphone)*0.1)
+st_symbian_sampled <- sample(st_symbian, length(st_symbian)*0.1)
+st_ios_sampled <- sample(st_ios, length(st_ios)*0.1)
+st_android_sampled <- sample(st_android, length(st_android)*0.1)
+
+
+df <- data.frame()
+x <- data.frame(st=servingtimes_sampled, type="total")
+df <- rbind(df,x)
+x <- data.frame(st=st_dongle_sampled, type="dongle")
+df <- rbind(df,x)
+x <- data.frame(st=st_regularphone_sampled, type="regular")
+df <- rbind(df,x)
+x <- data.frame(st=st_smartphone_sampled, type="smartphone")
+df <- rbind(df,x)
+x <- data.frame(st=st_symbian_sampled, type="symbian")
+df <- rbind(df,x)
+x <- data.frame(st=st_android_sampled, type="android")
+df <- rbind(df,x)
+x <- data.frame(st=st_ios_sampled, type="ios")
+df <- rbind(df,x)
+
+
+p <- ggplot(df, aes(x=st, color=as.factor(type), fill=as.factor(type))) + geom_density(adjust=0.5, size=1, alpha=0.3)
+p <- p + scale_x_log10(limits=c(0.2, 1000000)) + xlab("tunnel duration (s)")
+p <- p + annotation_logticks(sides="b")
+p <- p + scale_color_manual(values=cbPalette, name="") + scale_fill_manual(values=cbPalette, name="")
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p
+
+ggsave("R-duration-classification-density.pdf", width=12, height=10, useDingbats=F)
+embed_fonts("R-duration-classification-density.pdf")
+
+
+
+## histogram attempts, not really working yet
+histbreaks <- seq(0,max(servingtimes), max(servingtimes)/10000)
+
+p <- ggplot(df, aes(x=st, color=as.factor(type)))
+p <- p + geom_step(aes(st, y =..density..), stat = 'bin',breaks = histbreaks, position = "dodge")
+p + scale_x_log10(limits=c(0.01, 1000000)) + scale_y_log10()
 
---
+++ scripts/layer-timescales.R
@@ -0,0 +1,33 @@
+library(ggplot)
+
+
+xmin <- c(0.0000005, 0.000008, 0.005, 0.005, 0.05, 0.5, 0.5, 0.5, 0.6, 0.7, 10)
+xmax <- c(0.0008, 5, 0.5, 5, 5, 5, 3, 3, 20, 4000, 10000000)
+x <- c(100, 100, 100, 0.000001, 0.000001, 0.000001, 0.000001, 0.000001, 0.000001, 0.000001, 0.000001)
+
+ymin <- 0.8:10.8
+ymax <- 1.3:11.3
+type <- c("10Gbps to 100Mbps ethernet packet duration",
+          "wired RTT, ICMP ping, TCP bw probing, ECN",
+          "video content at 1Mbps per ethernet frame",
+          "LTE RTT",
+          "UMTS RTT",
+          "TCP initial RTO",
+          "RTP receiver report",
+          "HTTP adaptive streaming chunk",
+          "video buffers, application layer flow control",
+          "web session, RTP stream",
+          "GTP tunnel duration")
+
+df <- data.frame(x=x, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, type=type)
+
+p <- ggplot(df, aes(x=x, y=ymin+(ymax-ymin)/2, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, label=type)) + geom_rect()
+p <- p + scale_y_discrete(breaks=NULL) + annotation_logticks(sides="b")
+p <- p + theme(axis.ticks = element_blank(), axis.text.y = element_blank())
+p <- p + geom_text(hjust=0, family="Liberation Sans Narrow") + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p <- p + scale_x_log10(limits=c(0.0000001, 10000000), breaks=c(0.000001, 0.0001, 0.01, 1, 100, 10000, 1000000))
+p <- p + ylab("") + xlab("time (s)")
+p
+
+ggsave("layer-timescales.pdf", width=12, height=6, useDingbats=F)
+embed_fonts("layer-timescales.pdf")
