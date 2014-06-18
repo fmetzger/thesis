2bbad9b3de21446fcb6d7e5ca220652fee458e8c
Florian Metzger <florian.metzger@univie.ac.at>
2014-06-18 10:46:56 +0200

    replaced remaining 4 python plots with fancy new R ones font cleaning completed

---
+++ scripts/streaming-eval-plotbuffer-additive.r
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(grid) # needed for arrow
+library(extrafont)
+
+# used dataset: hPUGNCIozp0_delay_2500, subset 1
+# commandline: /ytresults_commandline.py hPUGNCIozp0_delay_2500 1 {nnbs,stbs,ytfa,ffh5} plotcumdata
+
+
+plotbuffer <- function(csvfilestring){
+  # precision too high for automatic numeric conversion, specify it manually
+  d <- read.csv(csvfilestring, header=T, sep=",", colClasses=c("numeric", "numeric", "character"))
+  
+  t1 <- subset(d, type=="frame")
+  diff <- diff(t1$size)
+  t1$diff <- c(0,diff)
+  
+  t2 <- subset(d, type=="segment")
+  diff <- diff(t2$size)
+  t2$diff <- c(0,diff)
+  
+  d <- rbind(t1,t2)
+  
+  d <- d[with(d, order(timestamp)), ]
+  
+  d$sum <- 0
+  
+  for(i in 1:nrow(d)) {
+    row <- d[i,]
+    #print(row)
+    if (i == 1) {
+      d[i,5] <- d[i,4]
+    } else {
+      if (d[i, 3] == "segment") {
+        d[i,5] <- d[i-1,5] + d[i,4]
+      } else {  # "frame"
+        d[i,5] <- d[i-1,5] - d[i,4]
+      }
+    }
+    # do stuff with row
+  }
+  
+  p <- ggplot(d, aes(x=timestamp, y=sum)) + geom_line(size = 1)
+  p <- p + xlab("time (s)") + ylab("buffered data (KiB)") 
+  p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+  return(p)
+}
+
+p <- plotbuffer("/home/fm/git/thesis/scripts/streaming-eval-null-strategy.csv")
+p
+ggsave("R-bufferlevel-stall.pdf", width=12, height=10, useDingbat=F)
+embed_fonts("R-bufferlevel-stall.pdf")
+
+
+p <- plotbuffer("streaming-eval-flash-strategy.csv")
+p
+ggsave("R-bufferlevel-stall.pdf", width=12, height=10, useDingbat=F)
+embed_fonts("R-bufferlevel-stall.pdf")
+
--- scripts/streaming-eval-plotbuffer-separate.r
+++ scripts/streaming-eval-plotbuffer-separate.r
@@ -2,16 +2,10 @@ library(ggplot2)
 library(grid) # needed for arrow
 library(extrafont)
 
-# auto string to numeric conversion seems to be broken here
-# timestamp and size are erroneously converted to factors
-# we do it manually instead
-d <- read.csv("/home/fm/git/thesis/scripts/buffer.csv", header=T, sep=",", as.is=T)
-d$timestamp <- as.numeric(d$timestamp)
-d$size <- as.numeric(d$size)
-d$type <- as.factor(d$type)
+# precision too high for automatic numeric conversion, specify it manually
+d <- read.csv("/home/fm/git/thesis/scripts/streaming-eval-blocktransfer.csv", header=T, sep=",", colClasses=c("numeric", "numeric", "factor"))
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
 
 p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line(size = 1)
 p <- p + xlab("time (s)") + ylab("data (KiB)") 
@@ -21,6 +15,7 @@ p <- p + scale_color_manual(values=cbPalette, name="",breaks=c("segment", "frame
 p
 #p + annotation_custom(grob=circleGrob(r=unit(1,"npc")), xmin=2, xmax=4, ymin=4000, ymax=4500) #  add circle on top for more highlighting
 ggsave("R-blocktransfer.pdf", width=12, height=10, useDingbat=F)
+embed_fonts("R-blocktransfer.pdf")
 
 p <- ggplot(d, aes(x=timestamp, y=size, color=type)) + geom_line(size = 1) +geom_point(size=3) + xlim(c(4.4,5.5)) + ylim(c(4350,4550))
 p <- p + xlab("time (s)") + ylab("data (KiB)")
@@ -28,3 +23,4 @@ p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
 p <- p + scale_color_manual(values=cbPalette, guide="none")
 p                              
 ggsave("R-blocktransferdetail.pdf", width=12, height=10, useDingbat=F)
+embed_fonts("R-blocktransferdetail.pdf")
