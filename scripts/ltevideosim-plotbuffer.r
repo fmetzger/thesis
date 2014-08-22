ffb8747d19b8b3108b7c60d936f858902cf60485
Florian Metzger <fm@tru42.org>
2014-08-22 15:25:44 +0200

    starting general testbeds description

--- scripts/ltevideosim-plotbuffer.r
+++ scripts/ltevideosim-plotbuffer.r
@@ -14,15 +14,29 @@ threshold.startplayback <- controls.log[1,6] # at or above
 threshold.startTransmission <- controls.log[1,4] # at or below
 
 
-df <- data.frame(timestamp <- buffer.log[,1], buffereddata <- buffer.log[,2])
+df <- data.frame(timestamp <- buffer.log[,1], buffer.data <- buffer.log[,2], buffer.time <- buffer.log[,5])
 
 
-p <- ggplot(df, aes(x=timestamp, y=buffereddata)) + geom_line(size=1)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
+p <- ggplot(df, aes(x=timestamp, y=buffer.data)) + geom_line(size=1)
 p <- p + xlab("time (s)") + ylab("buffered data (KiB)") 
 p
-		
-	plot(buffer.log[,1], buffer.log[,2], type="l", main="Buffer", xlab="Simulation Time [s]", ylab="Data [bytes]")
 
+
+p <- ggplot(df, aes(x=timestamp, y=buffer.time)) + geom_line(size=1)
+p <- p + geom_hline(yintercept = threshold.stopplayback, color="#E69F00", size=1)
+p <- p + geom_hline(yintercept = threshold.stoptransmission, color="#56B4E9", size=1)
+p <- p + geom_hline(yintercept = threshold.startplayback, color="#009E73", size=1)
+p <- p + geom_hline(yintercept = threshold.startTransmission, color="#F0E442", size=1)
+p <- p + xlab("time (s)") + ylab("buffered video duration (s)") 
+p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
+p
+ggsave("R-ltesim-plotbuffer-time.pdf", width=12, height=10, useDingbat=F)
+embed_fonts("R-ltesim-plotbuffer-time.pdf")
+
+		
 	plot(buffer.log[,1], buffer.log[,5], type="l", main="Buffer Time", xlab="Simulation Time [s]", ylab="Data [s]")
 	lines(controls.log[,1], controls.log[,4], col="blue")
 	lines(controls.log[,1], controls.log[,5], col="red")
