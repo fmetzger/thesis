6e0fd4066b9933fede6adf21ccc83286a82f3a01
Florian Metzger <florian.metzger@univie.ac.at>
2014-09-03 15:55:53 +0200

    simulation playback strategies and beginnings of scenarios

--- scripts/ltevideosim-plotbuffer.r
+++ scripts/ltevideosim-plotbuffer.r
@@ -2,7 +2,9 @@ library(ggplot2)
 library(extrafont)
 
 
-path <- "F:/uni/svn/fc-sim/results/set2/video1/100ms_1000mbit_0pct_results_1407771504"
+#path <- "/home/fm/svn/fc-sim/results/set2/video1/100ms_1000mbit_0pct_results_1407771504"
+path <- "/home/fm/svn/fc-sim/results/set3/video1/50ms_1000mbit_0pct_results_1409580670/"
+
 
 buffer.log <- read.table(sprintf("%s/%s", path, "buffer.log"), sep=" ", dec=".", header=F)
 controls.log <- read.table(sprintf("%s/%s", path, "controls.log"), sep=" ", dec=".", header=F)
@@ -27,25 +29,16 @@ p
 
 p <- ggplot(df, aes(x=timestamp, y=buffer.time)) + geom_line(size=1)
 p <- p + geom_hline(yintercept = threshold.stopplayback, color="#E69F00", size=1)
+p <- p + annotate("text", label = "playback stop", x = 520, y = threshold.stopplayback - 0.15, hjust = 1, family="Liberation Sans Narrow", size = 8)
 p <- p + geom_hline(yintercept = threshold.stoptransmission, color="#56B4E9", size=1)
+p <- p + annotate("text", label = "transmission stop", x = 520, y = threshold.stoptransmission - 0.15, hjust = 1, family="Liberation Sans Narrow", size = 8)
 p <- p + geom_hline(yintercept = threshold.startplayback, color="#009E73", size=1)
+p <- p + annotate("text", label = "playback start", x = 520, y = threshold.startplayback - 0.15, hjust = 1, family="Liberation Sans Narrow", size = 8)
 p <- p + geom_hline(yintercept = threshold.startTransmission, color="#F0E442", size=1)
+p <- p + annotate("text", label = "transmission start", x = 520, y = threshold.startTransmission - 0.15, hjust = 1, family="Liberation Sans Narrow", size = 8)
 p <- p + xlab("time (s)") + ylab("buffered video duration (s)") 
 p <- p + theme(text = element_text(family="Liberation Sans Narrow", size=20))
 p
 ggsave("R-ltesim-plotbuffer-time.pdf", width=12, height=10, useDingbat=F)
 embed_fonts("R-ltesim-plotbuffer-time.pdf")
-
-		
-	plot(buffer.log[,1], buffer.log[,5], type="l", main="Buffer Time", xlab="Simulation Time [s]", ylab="Data [s]")
-	lines(controls.log[,1], controls.log[,4], col="blue")
-	lines(controls.log[,1], controls.log[,5], col="red")
-	lines(controls.log[,1], controls.log[,6], col="green")
-	lines(controls.log[,1], controls.log[,7], col="yellow")
-	plot(buffer.log[,1], buffer.log[,6], type="l", main="Transmission and Playback Times", xlab="Simulation Time [s]", ylab="Data [s]")
-	lines(buffer.log[,1], buffer.log[,7], col="blue")
-	plot(buffer.log[,1], buffer.log[,3], type="l", main="Transmission and Playback Bytes", xlab="Simulation Time [s]", ylab="Data [bytes]")
-	lines(buffer.log[,1], buffer.log[,4], col="blue")
-	plot(ecdf(buffer.log[,7]/buffer.log[,6]), verticals=TRUE, main="Transmission and Playback Ratio", xlab="Played Video/Transmitted Video [%]", ylab="Frequency [%]")
-
 
